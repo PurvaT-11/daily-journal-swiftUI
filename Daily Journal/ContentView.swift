51b8e356677cc81b60d44e6a9969ca79bb0f4d7a
Purva <ptand@uic.edu>
2025-04-21 18:08:47 -0500

    Initial Commit

---
+++ Daily Journal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Daily Journal
+//
+//  Created by Purva Tandel on 4/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Daily Journal/Daily_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_JournalApp.swift
+//  Daily Journal
+//
+//  Created by Purva Tandel on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Daily_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
